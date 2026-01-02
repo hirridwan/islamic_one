f57456931a87fa2053f626587c15e3a6208c8f90
Ridwan Maulana <rmfhdofficial@gmail.com>
2026-01-02 10:58:07 +0700

    update

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -225,7 +225,7 @@ class SettingsScreen extends StatelessWidget {
           
           Center(
             child: Text(
-              "Simple Quran v1.0.1",
+              "Islamic One v1.0.1",
               style: TextStyle(color: Colors.grey[400], fontSize: 12),
             ),
           ),
